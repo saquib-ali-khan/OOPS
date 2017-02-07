944a1b28071646ec94c1759b6df30e81eb338e12
saquib-ali-khan <alikhansaquib@gmail.com>
2017-02-07 12:50:28 +0530

    Add files via upload

---
+++ saq5.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+class pntr_obj
+{
+	
+public: int roll_no;
+        string name;
+		void set_data(int a, string namee)
+		{
+			this->roll_no=a;
+			this->name=namee;
+			
+		};
+		void print(){
+			cout<<""
+			
+		};	
+	
+	
+	
+	
+	
+	
+};
