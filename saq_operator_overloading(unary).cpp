d8b042ec2c9e3aa816946ce9856e792db098038f
SAQUIB ALI KHAN <alikhansaquib@gmail.com>
2017-02-28 12:10:46 +0530

    Add files via upload

---
+++ saq_operator_overloading(unary).cpp
@@ -0,0 +1,78 @@
+// LAB Q.7 operator overloading(unary operator)
+
+// NOTE :  FOR OPERATOR OVERLOADING OF UNARY ++ WE HAVE TO DECLARE TWO DIFFERENT OVERLOADING FUNCTIONS IN THE CLASS DEFINITION.
+// ONE FOR THE PREFIX AND OTHER FOR THE POSTFIX
+
+
+#include<iostream>
+
+using namespace std;
+
+class bank{
+ static int bank_num;
+private:
+	  
+	   
+public:
+	    int num_person,bank_number;
+         
+	   bank(){
+	   	 num_person=0;
+	   	 bank_num++;
+	   	 this->bank_number=bank_num;
+	   	 
+	   }
+	   
+	    void print_num_person()
+	   {printf("The no. of persons inside the bank %d = %d",bank_number,num_person);
+			  }	 
+	   void operator ++ (){
+	   	num_person+=1;
+	   }
+	   void operator ++(int){     
+	   	num_person+=1;
+	   	
+	   }
+	   
+	   void operator -- (){
+	   	num_person-=1;
+	   }
+	   void operator --(int){
+	   	num_person-=1;
+	   }
+	   
+	   
+
+};
+int bank::bank_num=0;
+
+int main()
+{
+
+bank b1,b2,b3;
+
+b1.print_num_person();
+++b1;
+b1.print_num_person();
+
+--b1;
+b1.print_num_person();
+
+++b1;
+b1.print_num_person();
+--b1;
+b1.print_num_person();
+
+++b2;
+b2.print_num_person();
+
+b1++;
+b1.print_num_person();
+
+
+
+	
+	
+	
+	
+}
